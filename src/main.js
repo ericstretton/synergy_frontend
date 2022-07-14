ac869815e4f66f9c48326fab1f3cd5fbbf15bb74
ericstretton <eric.stretton@gmail.com>
2022-07-14 12:53:46 -0600

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {createPinia, PiniaVuePlugin} from 'pinia'
 import router from './router'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
+Vue.use(PiniaVuePlugin)
+const pinia = createPinia()
 
 new Vue({
+  pinia,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
